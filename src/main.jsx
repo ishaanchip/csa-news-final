dcad8eb739c7466367f7b4e6590069c886655af2
ishaanchip <ishaanchiplunkar@gmail.com>
2025-06-05 22:10:07 -0400

    checking changes rn

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//test
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
