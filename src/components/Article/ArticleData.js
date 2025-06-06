3937e3f07ac87518aa635ddc3c6acd5c2cf1a248
ishaanchip <ishaanchiplunkar@gmail.com>
2025-06-05 23:56:03 -0400

    add js file?

---
+++ src/components/Article/ArticleData.js
@@ -0,0 +1,201 @@
+//images & icons
+import testImg from "../../assets/comp.jpg"
+
+
+import sponserOne from "../../assets/fcc.jpg"
+
+
+
+//intercomponent imports
+
+
+//external dependenices
+
+
+export const articleOne = {
+    title:'Revolutionary AI Invention',
+    mini_description:'A groundbreaking AI invention is reshaping industries, boosting efficiency, enabling automation, and raising important questions about ethics and control.',
+    author:'ishaan chiplunkar',
+    release_date:'5/26/25',
+    img:testImg,
+    citation:"",
+    sponserOne:{
+        img:sponserOne,
+        link:"https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo:undefined,
+    text:[
+        {
+            header:'Revolutionary AI Invention Reshapes the Future of Work and Ethics',
+            content:["A groundbreaking artificial intelligence (AI) invention is transforming the modern world in ways once thought to belong only in science fiction. From automating complex tasks to enhancing human decision-making, this revolutionary AI system is not only boosting productivity across industries but also raising critical questions about ethics, human labor, and control.",
+                     "At the heart of this innovation is a new kind of AI architecture, one that combines self-learning algorithms with real-time adaptation. Unlike traditional models that rely heavily on pre-labeled datasets and rigid programming, this AI system learns dynamically from its environment and adapts continuously to new information—much like a human being. It's not just trained once and deployed; it evolves, refines its own models, and even identifies gaps in its own knowledge."
+                    ],
+        },
+        {
+            header:'Transforming Industry at Unprecedented Speed',
+            content:["The impact has been swift and sweeping. In the manufacturing sector, for example, this AI system has been deployed to manage supply chains, detect defects in products with near-perfect accuracy, and predict machinery failures before they occur—saving companies millions. In healthcare, it's being used to assist doctors in diagnosing rare diseases, designing personalized treatment plans, and managing hospital resources more efficiently.",
+                     "Finance, transportation, agriculture, and even creative fields like art and music are feeling the ripple effects. AI-generated music that responds in real time to audience emotions, news articles written in seconds based on live data, and autonomous delivery fleets—all powered by this technology—are already becoming reality."
+                    ],
+        },
+        {
+            header:'Ethical and Societal Implications',
+            content:["But this surge of efficiency and capability comes with new responsibilities. As AI systems take on more decision-making power, the question of who controls the controller becomes more urgent. Who is accountable when an AI makes a life-altering mistake—like misdiagnosing a patient or choosing to prioritize one delivery over another in an emergency?",
+                     "There’s also growing concern about job displacement. While some argue that AI will simply eliminate repetitive or dangerous work, others fear it could widen inequality and create large populations of workers with obsolete skills. Governments and industries now face the challenge of ensuring that AI complements human work instead of replacing it wholesale."
+                    ],
+        },
+        {
+            header:'A New Era Begins',
+            content:["Despite the challenges, the promise of this revolutionary AI invention is undeniable. It has the potential to solve problems that have eluded humanity for decades—from climate modeling and energy optimization to global food distribution and early disease detection. Yet its success will depend not just on how powerful the algorithms become, but on how thoughtfully society integrates them.",
+                     "This is not just a technological breakthrough—it’s a cultural one. As we stand at the dawn of the AI age, the choices we make today will shape the boundaries, possibilities, and values of tomorrow’s world. Will AI serve as a tool for collective progress or become a force that divides? That story is still being written—but the invention has already begun."
+                    ],
+        },
+
+
+    ]
+
+
+}
+
+export const articleTwo = {
+    title:"An Economist's Dream",
+    mini_description:"This article summarizes Behavioral Economist Senthil Mullainathan's love for the combination of economics, computer science, and new ideas. ",
+    author:'ishaan chiplunkar',
+    release_date:'5/28/25',
+    img:testImg,
+    citation:"",
+    sponserOne:{
+        img:sponserOne,
+        link:"https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo:undefined,
+    text:[
+        {
+            header:'Introduction',
+            content:["This article summarizes Behavioral Economist Senthil Mullainathan's love for the combination of economics, computer science, and new ideas.",]
+        },
+        {
+            header:'Greatness in the Making',
+            content:["As a child, Senthil Mullainathan explains that his dad lost his job as an aerospace engineer after coming to the US, so they had to survive by running a video store. He would study mathematics in his video store but found himself drawn to behavioral economics more than any other subject. ",
+                    "As a teen, he went to Cornell University and excelled in multiple different majors including computer science, economics, and mathematics. ",
+                    "He earned a PHD from Harvard and spent ten years working on studying people’s psychology, excelling himself farther into the field.“I think in the past era of science, science was done in big labs, and it was actioned into big things. I think the next age of science will be just as much about allowing individuals to rethink who they are and what their lives are like.”"
+            ]
+        },
+        {
+            header:'Current State',
+            content:[" Later into his career, Senthil started getting more interested in artificial intelligence and machine learning. “I wanted to be in a place where I could have one foot in computer science and one foot in a top-notch behavioral economics department,” he says. “And really, if you just objectively said ‘what are the places that are A-plus in both,’ MIT is at the top of that list.”He explains a possible use of AI in which a decision-maker, for example a judge or doctor, could have access to what their average decision would be related to a particular set of circumstances. Such an average would be potentially freer of day-to-day influences — such as a bad mood, indigestion, slow traffic on the way to work, or a fight with a spouse."
+            ]
+        },
+
+    ]
+}
+
+export const articleThree = {
+    title: "Orion AI",
+    mini_description: "Orion, an open-source tool that helps detect unusual patterns, or anomalies, in time-series data like sensor readings, health data, or cybersecurity logs.",
+    author: "ishaan chiplunkar",
+    release_date: "5/28/25",
+    img: testImg,
+    citation: "https://news.mit.edu/2025/anomaly-detection-framework-anyone-can-use-sarah-alnegheimish-0528",
+    sponserOne: {
+      img: sponserOne,
+      link: "https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo: undefined,
+    text: [
+      {
+        header: "Introduction",
+        content: [
+          "Sarah Alnegheimish, a PhD student at MIT, is working to make machine learning easier for everyone to use. She’s the creator of Orion, an open-source tool that helps detect unusual patterns, or anomalies, in time-series data like sensor readings, health data, or cybersecurity logs. Orion doesn’t require users to be machine learning experts—it includes easy commands and full transparency so anyone can follow how the models work. Inspired by her background and early use of MIT OpenCourseWare, Sarah believes that making tools open and accessible is the key to having a real impact."
+        ]
+      },
+      {
+        header: "Orion at Work",
+        content: [
+          "Orion has already helped many public users and has been downloaded over 120,000 times. Now, in her PhD work, she’s exploring how pre-trained models—which usually just make predictions—can be repurposed to find anomalies without extra training. She’s also tested using a chatbot-style interface so users can interact with Orion easily using simple commands like “Fit” and “Detect.” For Sarah, it’s not just about creating new AI models, but also about building systems that make those models usable by real people. Her work is helping move AI beyond the lab and into the hands of anyone who needs it."
+        ]
+      }
+    ]
+  }
+  
+
+export const articleFour = {
+    title: "Themis AI",
+    mini_description: "Themis AI, started by MIT researchers including Professor Daniela Rus, is working to make AI more trustworthy by helping models know when they don’t know something.",
+    author: "ishaan chiplunkar",
+    release_date: "6/3/25",
+    img: testImg,
+    citation: "https://news.mit.edu/2025/themis-ai-teaches-ai-models-what-they-dont-know-0603",
+    sponserOne: {
+      img: sponserOne,
+      link: "https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo: undefined,
+    text: [
+      {
+        header: "Themis AI",
+        content: [
+          "Themis AI, started by MIT researchers including Professor Daniela Rus, is working to make AI more trustworthy by helping models know when they don’t know something. Their platform, Capsa, wraps around any machine learning model and detects when the model is unsure, biased, or working with incomplete data. This is super important because AI is now being used in serious areas like self-driving cars, drug discovery, and customer support — and mistakes could be dangerous or costly. The team behind Themis AI has used Capsa to help companies in telecom, oil and gas, and even in making better chatbots. They’ve also worked on removing bias in facial recognition and improving accuracy in drug research. One of the coolest things is that Capsa can help smaller AI models used on phones or edge devices work more reliably without needing a big server. The company is now helping businesses that use large language models (LLMs) get more reliable results by letting the models report how confident they are in their answers. Themis AI is also looking into helping LLMs think more clearly using something called chain-of-thought reasoning. Overall, Themis AI is all about making AI smarter, safer, and more dependable in the real world."
+        ]
+      }
+    ]
+  }
+  
+
+export const articleFive = {
+    title: "Alzheimer’s Research",
+    mini_description: "MIT researchers, led by Professor Ernest Fraenkel, have discovered new potential targets for treating Alzheimer’s disease by analyzing large datasets from both humans and fruit flies.",
+    author: "ishaan chiplunkar",
+    release_date: "5/20/25",
+    img: testImg,
+    citation: "https://news.mit.edu/2025/scientists-discover-potential-new-targets-alzheimers-drugs-0520",
+    sponserOne: {
+      img: sponserOne,
+      link: "https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo: undefined,
+    text: [
+      {
+        header: "Introduction",
+        content: [
+          "MIT researchers, led by Professor Ernest Fraenkel, have discovered new potential targets for treating Alzheimer’s disease by analyzing large datasets from both humans and fruit flies."
+        ]
+      },
+      {
+        header: "Developing Research",
+        content: [
+          "Working with Harvard Medical School, the team identified over 200 genes linked to neurodegeneration and used network algorithms to find related cellular pathways. Two key pathways stood out: one related to RNA modification and the other to DNA damage repair — neither previously connected to Alzheimer’s in this way. When certain genes in these pathways were removed, neurons became more vulnerable to Alzheimer’s effects like Tau tangles and DNA damage. The study supports the idea that Alzheimer’s is caused by multiple factors, not just amyloid plaques, which most current drugs target. Using induced pluripotent stem cells (IPSCs) from Alzheimer’s patients, researchers hope to test new treatments based on these discoveries. The team believes that combining better lab models with advanced data analysis will help lead to breakthroughs in drug development. The study was published in Nature Communications and funded by the NIH."
+        ]
+      }
+    ]
+  }
+  
+
+export const articleSix = {
+    title: "Predicting Rare Failures",
+    mini_description: "MIT researchers have developed a new system to help predict and understand rare, large-scale failures in complex automated systems — like the Southwest Airlines scheduling meltdown during the 2022 holiday season.",
+    author: "ishaan chiplunkar",
+    release_date: "5/21/25",
+    img: testImg,
+    citation: "https://news.mit.edu/2025/learning-how-predict-rare-kinds-failures-0521",
+    sponserOne: {
+      img: sponserOne,
+      link: "https://www.freecodecamp.org/learn"
+    },
+    sponserImgTwo: undefined,
+    text: [
+      {
+        header: "Introduction",
+        content: [
+          "MIT researchers have developed a new system to help predict and understand rare, large-scale failures in complex automated systems — like the Southwest Airlines scheduling meltdown during the 2022 holiday season. Led by PhD student Charles Dawson and Professor Chuchu Fan, the team created an algorithm that uses a small amount of failure data and a lot of normal data to work backward and figure out what went wrong. The goal is to prevent future failures in systems where software and real-world operations interact, such as airline scheduling or autonomous vehicles."
+        ]
+      },
+      {
+        header: "Uncovering the Issue",
+        content: [
+          "Their research focused on how a snowstorm in Denver triggered a chain reaction that disrupted flights nationwide. By analyzing public flight data, they found that the placement and use of reserve aircraft played a big role. While some airports bounced back, others like Las Vegas had no weather issues but still suffered because planes got stuck elsewhere. The team’s system helped uncover this hidden pattern by estimating how reserve planes moved through the network.",
+          "Their method, now available as a tool called CalNF, can help organizations spot early signs of system trouble. The team hopes this work leads to real-time monitoring systems that warn of failures before they happen, helping avoid costly disasters."
+        ]
+      }
+    ]
+  }
+  
